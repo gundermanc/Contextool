4614dfadfaa2527cd85a6bb764656b2e4ddaf32a
Christian Gunderman <gundermanc@gmail.com>
2018-06-16 16:00:56 -0700

    Logging output pane and configuration file loader.

---
+++ src/Contextool.VSWindows/Common/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Contextool.Editor.Common
+{
+    using System.IO;
+    using System.Runtime.Serialization;
+    using System.Runtime.Serialization.Json;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.Threading;
+
+    [DataContract]
+    internal sealed class Configuration
+    {
+        public static async Task<Configuration> ReadFromFileAsync(string configurationFile)
+        {
+            // Ensure we're off the UI thread;
+            await TaskScheduler.Default;
+
+            using (var file = File.OpenRead(configurationFile))
+            {
+                return new DataContractJsonSerializer(typeof(Configuration)).ReadObject(file) as Configuration;
+            }
+        }
+    }
+}
---
+++ src/Contextool.VSWindows/TextViewCreationListener.cs
@@ -0,0 +1,99 @@
+﻿namespace Contextool.VSWindows
+{
+    using System;
+    using System.ComponentModel.Composition;
+    using System.IO;
+    using System.Threading.Tasks;
+    using Contextool.Editor.Common;
+    using Contextool.VSWindows.Common;
+    using Microsoft.VisualStudio.Text;
+    using Microsoft.VisualStudio.Text.Editor;
+    using Microsoft.VisualStudio.Threading;
+    using Microsoft.VisualStudio.Utilities;
+
+    [Export(typeof(ITextViewCreationListener))]
+    [Name(Name)]
+    [ContentType("any")]
+    [TextViewRole(PredefinedTextViewRoles.PrimaryDocument)]
+    internal sealed class TextViewCreationListener : ITextViewCreationListener
+    {
+        private const string Name = "Contextool TextView Creation Listener";
+        private const string ConfigurationFileName = "contextool.json";
+        private JoinableTaskContext joinableTaskContext;
+        private ITextDocumentFactoryService textDocumentFactoryService;
+        private readonly Logger logger;
+
+        [ImportingConstructor]
+        public TextViewCreationListener(
+            JoinableTaskContext joinableTaskContext,
+            ITextDocumentFactoryService textDocumentFactoryService,
+            Logger logger)
+        {
+            this.joinableTaskContext = joinableTaskContext
+                ?? throw new ArgumentNullException(nameof(joinableTaskContext));
+            this.textDocumentFactoryService = textDocumentFactoryService
+                ?? throw new ArgumentNullException(nameof(textDocumentFactoryService));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public void TextViewCreated(ITextView textView)
+        {
+            if (this.TryGetDocumentFilePath(textView.TextBuffer, out var filePath) &&
+                this.TryFindConfigurationFilePathForFile(filePath, out var configurationFilePath))
+            {
+                // TODO: load configuration only once.
+                this.joinableTaskContext.Factory.RunAsync(async delegate
+                {
+                    try
+                    {
+                        var configuration = await Configuration.ReadFromFileAsync(configurationFilePath);
+                    }
+                    catch (Exception ex)
+                    {
+                        this.logger.LogException(ex);
+                    }
+
+                });
+            }
+            else
+            {
+                this.logger.LogMessage(
+                    string.Format(
+                        Strings.ConfigurationNotFoundFormat,
+                        ConfigurationFileName,
+                        filePath));
+            }
+        }
+
+        private bool TryFindConfigurationFilePathForFile(string filePath, out string configurationFilePath)
+        {
+            while (true)
+            {
+                filePath = Path.GetDirectoryName(filePath);
+                if (string.IsNullOrWhiteSpace(filePath))
+                {
+                    configurationFilePath = string.Empty;
+                    return false;
+                }
+
+                configurationFilePath = Path.Combine(filePath, ConfigurationFileName);
+                if (File.Exists(configurationFilePath))
+                {
+                    return true;
+                }
+            }
+        }
+
+        private bool TryGetDocumentFilePath(ITextBuffer textBuffer, out string filePath)
+        {
+            if (this.textDocumentFactoryService.TryGetTextDocument(textBuffer, out var document))
+            {
+                filePath = document.FilePath;
+                return true;
+            }
+
+            filePath = string.Empty;
+            return false;
+        }
+    }
+}
